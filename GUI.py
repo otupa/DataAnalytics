f9a29c97260f4ead3f3f145eb5b52ef239ce9f6b
MarianoTupa <tupa.sales@gmail.com>
2021-02-01 22:38:56 -0300

    Atualização frontend

--- GUI.py
+++ GUI.py
@@ -12,19 +12,11 @@ window = Tk()
 
 class Funcoes():
 
-    # Fornece dados dos motoristas para o dropdawn
     def list_moto(self):
         list_ = []
         arquivos = os.listdir('data_csv/')
         [list_.append(str(i)[:-4].replace(" ", "_")) for i in arquivos]
         return list_
-
-    # Limpa as entrys
-    def limpar(self):
-        self.lb_codigo_entry.delete(0, END)
-        self.lb_nome_entry.delete(0, END)
-        self.lb_telefone_entry.delete(0, END)
-        self.lb_cidade_entry.delete(0, END)
 
     # Backend Data Screpping e Criação do banco de dados
     def data_screrping(self):
@@ -38,10 +30,12 @@ class Funcoes():
     # Conectar ao banco de dados
     def connect_sql(self):
         self.connect = Connect()
+        return True
 
     # desconectar do banco de dados
     def disconect_sql(self):
         self.connect.close_db()
+        return True
 
     # Selecionar tabela
     def select_tb(self):
@@ -54,10 +48,10 @@ class Funcoes():
             self.listaCli.insert("", END, values=info)
         self.disconect_sql()
 
-
-
-
-
+    # recebe a string do dropdawn
+    def drop_data(self, selection):
+        print(selection)
+        return True
         
 
 class Application(Funcoes):
@@ -65,14 +59,18 @@ class Application(Funcoes):
     def __init__(self):
         self.data_screrping()
         self.window = window
+
         self.tela()
         self.frames_tela()
-        self.widgets_frame_1()
-        self.list_frame_2()
+        self.textos()
+        self.menu_moto()
+        self.calendario()
+        self.botoes()
+        self.Treeview_frame_1()
+        self.Treeview_frame_2()
         self.select_tb()
+
         window.mainloop()
-
-
 
     def tela(self):
         self.window.title("Soft G4")
@@ -84,104 +82,115 @@ class Application(Funcoes):
 
     def frames_tela(self):
 
-        # Criando Frames
-        self.frame_1 = Frame(self.window, bd=4, bg='#dfe3ee',
-            highlightbackground='#759fe6', highlightthickness=3)
-        self.frame_2 = Frame(self.window, bd=4, bg='#dfe3ee',
-            highlightbackground='#759fe6', highlightthickness=3)
+        # Frame que exibe as funções
+        self.frame_1 = Frame(
+            self.window, 
+            bd=4, 
+            bg='#dfe3ee',
+            highlightbackground='#759fe6', 
+            highlightthickness=3
+            )
 
-        # Empacotando Frames de forma responsiva
-        self.frame_2.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
-        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46)
+        # Frame que exibe o banco de dados
+        self.frame_2 = Frame(
+            self.window, 
+            bd=4, 
+            bg='#dfe3ee',
+            highlightbackground='#759fe6', 
+            highlightthickness=3
+            )
 
-    def widgets_frame_1(self):
+        # Empacotando de forma responsiva
+        self.frame_1.place(
+            relx=0.02, 
+            rely=0.02, 
+            relwidth=0.96,
+            relheight=0.46
+            )
 
-        # Labels e entrada do codigo
-        self.lb_codigo = Label(self.frame_1, text="Código", bg='#dfe3ee')
-        self.lb_codigo.place(relx=0.05, rely=0.05)
+        # Empacotando de forma responsiva
+        self.frame_2.place(
+            relx=0.02, 
+            rely=0.5, 
+            relwidth=0.96, 
+            relheight=0.46
+            )
 
-        self.lb_codigo_entry = Entry(self.frame_1)
-        self.lb_codigo_entry.place(relx=0.05, rely=0.15, relwidth=0.08)
+    def menu_moto(self):
 
-        # Label e entrada do nome
-        self.lb_nome = Label(self.frame_1, text="nome", bg='#dfe3ee')
-        self.lb_nome.place(relx=0.05, rely=0.35)
-
-        self.lb_nome_entry = Entry(self.frame_1)
-        self.lb_nome_entry.place(relx=0.05, rely=0.45, relwidth=0.80)
-
-        # Label e entrada do telefone  
-        self.lb_telefone = Label(self.frame_1, text="telefone", bg='#dfe3ee')
-        self.lb_telefone.place(relx=0.05, rely=0.55)
-
-        self.lb_telefone_entry = Entry(self.frame_1)
-        self.lb_telefone_entry.place(relx=0.05, rely=0.65, relwidth=0.25)
-
-        # Label e entrada do cidade
-        self.lb_cidade = Label(self.frame_1, text="cidade", bg='#dfe3ee')
-        self.lb_cidade.place(relx=0.35, rely=0.55)
-
-        self.lb_cidade_entry = Entry(self.frame_1)
-        self.lb_cidade_entry.place(relx=0.35, rely=0.65, relwidth=0.5)
-
-
-
-        # Botão limpar
-        self.bt_limpar = Button(self.frame_1, text="limpar", bd=2, bg='#76BCDD', 
-            fg='white', font=('verdana', 7, 'bold'), command=self.limpar)
-        self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
-
-        # Botão buscar
-        self.bt_buscar = Button(self.frame_1, text="buscar", bd=2, bg='#76BCDD', 
-            fg='white', font=('verdana', 7, 'bold'))
-        #self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
-
-        # Botão novo
-        self.bt_novo = Button(self.frame_1, text="novo", bd=2, bg='#76BCDD', 
-            fg='white', font=('verdana', 7, 'bold'))
-        self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
-
-        # Botão alterar
-        self.bt_alterar = Button(self.frame_1, text="alterar", bd=2, bg='#76BCDD', 
-            fg='white', font=('verdana', 7, 'bold'))
-        self.bt_alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
-
-        # Botão apagar
-        self.bt_apagar = Button(self.frame_1, text="apagar", bd=2, bg='#76BCDD', 
-            fg='white', font=('verdana', 7, 'bold'))
-        self.bt_apagar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
-
-
-
-
-        # Opções do dropdawn
         options = self.list_moto()
 
-        # Criar dropdawn
-        self.dropdawn = StringVar()
-        self.dropdawn.set("motorista")
+        self.drop_ = StringVar()
 
-        # Configurar e alocar dropdawn
-        self.drop = OptionMenu(self.window, self.dropdawn, *options)
-        self.drop.place(relx=0.35, rely=0.1, relwidth=0.2, relheight=0.05)
+        self.drop_.set("MOTORISTA")
 
-        # Criar Calendarios
+        self.drop = OptionMenu(self.frame_1, self.drop_, *options, command=self.drop_data)
 
+        self.drop.place(relx=0.05, rely = 0.1, relwidth = 0.40, relheight = 0.15)
 
-
-
-
-
-
-
-
-
-    def list_frame_2(self):
-
-        self.scroll_list = Scrollbar(self.frame_2, orient='vertical')
+    def calendario(self):
         
-        self.listaCli = ttk.Treeview(self.frame_2, height=3, column=("coll1", "coll2", "coll3", "coll4"), yscroll=self.scroll_list)
+        # Criando calendario
+        self.calendar_1 = DateEntry(self.frame_1, width=12, background='#3D51C3', foreground='white', 
+            borderwidth=2, font="Arial 12", selectmode='day', cursor="hand1", year=2020, month=2, day=5)
+
+        self.calendar_2 = DateEntry(self.frame_1, width=12, background='#3D51C3',foreground='white', 
+            borderwidth=2, font="Arial 12", selectmode='day', cursor="hand1", year=2020, month=2, day=5)
+
+        self.calendar_1.place(relx=0.05, rely=0.35, relwidth=0.19, relheight=0.1)
+
+        self.calendar_2.place(relx=0.26, rely=0.35, relwidth=0.19, relheight=0.1)
+
+    def botoes(self):
+
+        self.bt_pesquisar = Button(self.frame_1, text="PESQUISAR", bd=2, 
+            bg='#364094', fg='white', font=('verdana', 10, 'bold'), command=None)
+
+        self.bt_export_pdf = Button(self.frame_1, text="EXPORTAR PDF", bd=2, 
+            bg='#364094', fg='white', font=('verdana', 8, 'bold'), command=None)
+
+        self.bt_exportar_todos = Button(self.frame_1, text="EXPORTAR TUDO", bd=2, 
+            bg='#D92A2A', fg='white', font=('verdana', 8, 'bold'), command=None)
+
+        self.bt_pesquisar.place(relx=0.05, rely=0.5, relwidth=0.4, relheight=0.2)
+
+        self.bt_export_pdf.place(relx=0.05, rely=0.725, relwidth=0.19, relheight=0.15)
+
+        self.bt_exportar_todos.place(relx=0.26, rely=0.725, relwidth=0.19, relheight=0.15)
+
+    def textos(self):
+
+        self.lb_motorista = Label(self.frame_1, text="Escolha o motorista: ", bg='#dfe3ee', font=('Arial', 10, 'bold'))
+
+        self.lb_resultado = Label(self.frame_1, text="Analise dos resultados", bg='#dfe3ee', font=('Arial', 10, 'bold'))
+
+        self.lb_data_inicial = Label(self.frame_1, text = "Data inicial:", bg = '#dfe3ee')
+
+        self.lb_data_final = Label(self.frame_1, text = "Data final:", bg = '#dfe3ee')
+
+
+        self.lb_motorista.place(relx=0.05, rely=0.005)
+
+        self.lb_resultado.place(relx=0.5, rely=0.005)
+
+        self.lb_data_inicial.place(relx = 0.05, rely = 0.27)
+
+        self.lb_data_final.place(relx = 0.26, rely = 0.27)
+
+    def Treeview_frame_1(self):
+        self.Resultado = ttk.Treeview(self.frame_1)
         
+        self.Resultado.place(relx=0.50, rely=0.1, relwidth=0.435, relheight=0.785)
+
+    def Treeview_frame_2(self):
+
+        self.listaCli = ttk.Treeview(self.frame_2, height=3, 
+            column=("coll1", "coll2", "coll3", "coll4"))
+
+        self.scroll_list = Scrollbar(self.frame_2, orient='vertical', command=self.listaCli.yview)
+
+        self.listaCli.configure(yscrollcommand=self.scroll_list.set)
+
         self.listaCli.heading("#0", text="")
         self.listaCli.heading("#1", text="cod")
         self.listaCli.heading("#2", text="data")
@@ -195,6 +204,7 @@ class Application(Funcoes):
         self.listaCli.column("#4", width=45)
 
         self.listaCli.place(relx=0.01, rely=0.1, relwidth=0.95, relheight=0.85)
+
         self.scroll_list.place(relx=0.96, rely=0.1, relwidth=0.04, relheight=0.85)
 
 
@@ -202,26 +212,3 @@ a = Application()
 
 
 
-
-
-        #def calendar_view():
-"""            def print_sel():
-                print(cal1.selection_get(), cal2.selection_get())
-
-            cal1 = Calendar(self.frame_data, font="Arial 14", selectmode='day', cursor="hand1", year=2020, month=2, day=5)
-            cal2 = Calendar(self.frame_data, font="Arial 14", selectmode='day', cursor="hand1", year=2020, month=2, day=5)
-
-            cal1.pack(fill="both", expand=True)
-            cal2.pack(fill="both", expand=True)
-
-            ttk.Button(self.frame_data, text="ok", command=print_sel).pack()
-
-        def dateentry_view():
-            def print_sel(e):
-                print(cal.get_date())
-
-            ttk.Label(self.window, text='Choose date').pack(padx=10, pady=10)
-            cal = DateEntry(self.frame_data, width=12, background='darkblue',
-                            foreground='white', borderwidth=2)
-            cal.pack(padx=10, pady=10)
-            cal.bind("<return>", print_sel)"""
