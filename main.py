5ed45e17f90c538be86b9e3bcc92d1a83c5c226e
MarianoTupa <tupa.sales@gmail.com>
2021-02-21 00:56:18 -0300

    script main criado (#001)

--- main.py
+++ main.py
@@ -1,47 +1,15 @@
-import re
-import os
-import pandas as pd
+def iniciar():
 
-class SoftG4:
+    print()
 
-	dir_csv = 'data_csv/'
-	dir_conversas = 'conversas/'
-	data_frame = []
-
-	# Função usada para filtrar e tratar as informações.
-	def filtrar_conversa(self, arg):
-
-		# Separa apenas as informações desejadas
-		data = re.findall(r"\d+/\d+/\d+", arg) 
-		hora = re.findall(r"\d+\:\d+", arg)
-		valor = re.findall(r"\d+\sreais", arg)
-		r = [data, hora, valor]
-
-		# Remove caracteres indesejados
-		for i, j in enumerate(r):
-			r[i] = j[0].replace(',','').replace('!','').replace('.','')
-
-		# Incere os dados em uma lista usada pelo Pandas
-		data_frame.append(r)
-
-	def filtrar(self, file):
-		
-		# Abre a conversa e cria uma lista que separa cada linha.
-		conversa = open(dir_conversas+file, 'r', encoding = 'utf-8').read().splitlines()
-
-		# Captura o nome que será usado para salvar o arquivo
-		nome_csv = os.path.basename(dir_conversas+file)[:-4][25:]
-
-		# Filtra apenas as as mensagens desejadas.
-		data_conversa = [linha for linha in conversa if 'reais' in linha]
-
-		# Estrutura de laço que invoca a função filtrar.
-		filtro = [filtrar_conversa(str(i)) for i in data_conversa]
-
-		# Cria e salva um DataFrame Pandas em um arquico csv.
-		df = pd.DataFrame(data_frame).to_csv(dir_csv+nome_csv+'.csv', header=['data', 'hora', 'valor'], encoding='utf-8', index=False)
-		data_frame.clear() #Fecha o DataFrame
+    print(
+        "   ##################################",
+        "\n",
+        "  - - - - - - - SoftG4 - - - - - - -",
+        "\n",
+        "  ##################################",
+        "\n",
+        )
 
 if __name__ == '__main__':
-
-
+    iniciar()
